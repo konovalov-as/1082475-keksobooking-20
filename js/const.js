cef1295674b68ab298d5ee9041828a9c330d534c
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-07-18 16:06:43 +0500

    Добавляет мелкие исправления

--- js/const.js
+++ js/const.js
@@ -3,7 +3,6 @@
 (function () {
   var MOUSE_LEFT_BUTTON = 0;
   var PIN_COUNT = 5;
-  var MAP_BLOCK_ELEMENT_CONT = 2;
   var PinSize = {
     SIDE_LENGTH: 62,
     HEIGHT: 84,
@@ -18,7 +17,6 @@
   window.const = {
     MOUSE_LEFT_BUTTON: MOUSE_LEFT_BUTTON,
     PIN_COUNT: PIN_COUNT,
-    MAP_BLOCK_ELEMENT_CONT: MAP_BLOCK_ELEMENT_CONT,
     PinSize: PinSize,
     Key: Key,
   };
